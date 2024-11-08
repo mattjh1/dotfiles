7908e5ed7c36401fac364caac2edc694cd08e9a4
Mattias Holmgren <mattias.holmgren46@gmail.com>
2024-11-08 23:35:30 +0100

    fix some deprecated opts in obsidian.nvim

--- dotfiles/dot_config/nvim/lua/plugins/obsidian.lua
+++ dotfiles/dot_config/nvim/lua/plugins/obsidian.lua
@@ -14,33 +14,26 @@ local options = {
 		folder = "vault/dailies",
 		template = "log-template.md",
 	},
+	-- Where to put new notes created from completion. Valid options are
+	--  * "current_dir" - put new notes in same directory as the current buffer.
+	--  * "notes_subdir" - put new notes in the default notes subdirectory.
+	new_notes_location = "current_dir",
 	completion = {
 		-- Set to false to disable completion.
 		nvim_cmp = true,
 
 		-- Trigger completion at 2 chars.
 		min_chars = 2,
-
-		-- Where to put new notes created from completion. Valid options are
-		--  * "current_dir" - put new notes in same directory as the current buffer.
-		--  * "notes_subdir" - put new notes in the default notes subdirectory.
-		new_notes_location = "current_dir",
-
-		-- Control how wiki links are completed with these (mutually exclusive) options:
-		--
-		-- 1. Whether to add the note ID during completion.
-		-- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
-		-- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-		prepend_note_id = true,
-		-- 2. Whether to add the note path during completion.
-		-- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
-		-- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
-		prepend_note_path = false,
-		-- 3. Whether to only use paths during completion.
-		-- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
-		-- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-		use_path_only = false,
 	},
+	wiki_link_func = function(opts)
+		if opts.id == nil then
+			return string.format("[[%s]]", opts.label)
+		elseif opts.label ~= opts.id then
+			return string.format("[[%s|%s]]", opts.id, opts.label)
+		else
+			return string.format("[[%s]]", opts.id)
+		end
+	end,
 	open_app_foreground = true,
 	-- Optional, customize how names/IDs for new notes are created.
 	note_id_func = function(title)
